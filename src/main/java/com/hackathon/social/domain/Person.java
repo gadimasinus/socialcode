b57b94272c395f2a41b97b845e7adf076ae7b99a
Manish <gadimasinus@yahoo.com>
2023-09-17 17:59:55 -0400

    added person id

--- src/main/java/com/hackathon/social/domain/Person.java
+++ src/main/java/com/hackathon/social/domain/Person.java
@@ -1,12 +1,21 @@
 package com.hackathon.social.domain;
 
 public class Person {
+	private long id;
 	private String name;
 	private String address;
 
 	public Person(String name, String address) {
 		this.name = name;
 		this.address = address;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -17,7 +26,10 @@ public class Person {
 		return address;
 	}
 
+	@Override
 	public String toString() {
-		return name + "(" + address + ")";
+		return "Person [id=" + id + ", name=" + name + ", address=" + address + "]";
 	}
+
+   
 }
